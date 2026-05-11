507ce4f6fb27466c5fedf768a50c50b59a0bdcf5
vinayakmalviya21 <vinayakmalviya21@gmail.com>
2026-05-11 16:57:04 +0530

    updated user.java

--- src/main/java/com/expense/demo/entity/User.java
+++ src/main/java/com/expense/demo/entity/User.java
@@ -1,17 +1,18 @@
 package com.expense.demo.entity;
+
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+
+import lombok.Data;
 
 @Entity
-@Getter @Setter
+@Data
+@Table(name = "users")
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-
-    @Column(unique = true)
     private String email;
 
     private String password;
