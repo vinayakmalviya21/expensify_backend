474b1b8f7257e942352c327b07c8e1aecc9e6cd4
vinayakmalviya21 <vinayakmalviya21@gmail.com>
2026-05-11 17:14:56 +0530

    user.java modified

--- src/main/java/com/expense/demo/config/SecurityConfig.java
+++ src/main/java/com/expense/demo/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/",
-                                "/api/auth/**")
+                                "/auth/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
--- src/main/java/com/expense/demo/entity/User.java
+++ src/main/java/com/expense/demo/entity/User.java
@@ -13,6 +13,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
+
+    @Column(unique = true)
     private String email;
 
     private String password;
